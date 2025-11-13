67f4f63a05ccd42265a6ce613a06a766bd513353
tomercarolla <carollatomer@gmail.com>
2025-11-13 22:34:09 +0200

    add utils

---
+++ src/utils/is-valid-ip.tsx
@@ -0,0 +1,4 @@
+export const isValidIp = (ip: string): boolean => {
+    const regex = /^(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)$/;
+    return regex.test(ip);
+};
